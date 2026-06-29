e8bded7de23330d1e848585d0e6bef5e7ec71522
Bindu S C <bindusc2@gmail.com>
2026-06-29 12:31:53 +0530

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,11 @@
+uniquota = {"subscribers":10000, "views":3000, "likes":500}
+print(uniquota)
+uniquota["subscribers"] = 13000
+print(uniquota)
+print(uniquota["subscribers"])
+print(uniquota["views"])
+print(uniquota["likes"])
+print(uniquota)
+
+
+
